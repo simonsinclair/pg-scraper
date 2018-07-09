7d14d626b2a5566a7818c96bee2670aa6f46d6d0
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-07-09 23:52:14 +0100

    Add num and total sample fields to Metric model

--- models/Metric.js
+++ models/Metric.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 let metric = new mongoose.Schema({
-  timestamp_minute: Date,
+  timestamp_hour: Date,
+
+  // Support averaging.
+  num_samples: Number,
+  total_samples: Number,
+
   type: String,
+
+  // Data resolution - per minute.
   values: [ Number ],
 });
 
