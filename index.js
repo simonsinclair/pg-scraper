0c6327e9afe010de50eea52a38c8b6627431a9d9
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-07-12 23:21:52 +0100

    Refactor

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const CREDENTIALS = require('./credentials.js');
 
 const havePreviousSession = fs.existsSync('./session.json');
 
-// APP
-//////
 
-// DOM Selectors
-//
+/////////
+// APP //
+/////////
+
 
 // /login/
 //
+
+const LOGIN_URL_REGEX = RegExp('.*\.puregym\.com\/login', 'i');
 
 const USERNAME_SELECTOR = '#email';
 const PASSWORD_SELECTOR = '#pin';
@@ -39,52 +41,71 @@ async function run() {
 
   const page = await browser.newPage();
 
+  // Check if we have a previous session we can load.
   if (havePreviousSession) {
-    const cookies = await loadJsonFile('./session.json');
 
-    if (cookies.length !== 0) {
-      for (let cookie of cookies) {
-        await page.setCookie(cookie);
-      }
+    await loadPreviousSessionCookies(page);
+    const membersPageResponse = await page.goto('https://www.puregym.com/members/');
+
+    // If we got returned to the login page, then login.
+    if (LOGIN_URL_REGEX.test(membersPageResponse.url())) {
+      await login(page);
     }
-
-    await page.goto('https://www.puregym.com/members/');
 
   } else {
 
-    // Login
-    //
+    await login(page);
 
-    await page.goto('https://www.puregym.com/login/');
-
-    await page.click(USERNAME_SELECTOR);
-    await page.keyboard.type(CREDENTIALS.username);
-
-    await page.click(PASSWORD_SELECTOR);
-    await page.keyboard.type(CREDENTIALS.password);
-
-    await page.click(LOGIN_SUBMIT_SELECTOR);
-
-    await page.waitForNavigation();
-
-    // Store session
-    const cookiesObject = await page.cookies();
-    await writeJsonFile('./session.json', cookiesObject, { indent: 2 });
   }
 
-
-  // /members
-  //
-
-  // Capture number of people
-  const numPeopleInMyGym = await page.evaluate((selector) => {
-    const element = document.querySelector(selector);
-    return element.innerHTML.split(' ')[0];
-  }, N_PEOPLE_SELECTOR);
-
+  // Get people numbers!
+  const numPeopleInMyGym = await getNumPeopleInMyGym(page);
   console.log(numPeopleInMyGym);
 
   browser.close();
 }
 
 run();
+
+
+// Functions
+//
+
+async function loadPreviousSessionCookies(page) {
+  const cookies = await loadJsonFile('./session.json');
+
+  if (cookies.length !== 0) {
+    for (const cookie of cookies) {
+      await page.setCookie(cookie);
+    }
+  }
+}
+
+async function saveSessionCookies(page) {
+  const cookiesObject = await page.cookies();
+  await writeJsonFile('./session.json', cookiesObject, { indent: 2 });
+}
+
+async function login(page) {
+  await page.goto('https://www.puregym.com/login/');
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDENTIALS.username);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDENTIALS.password);
+
+  await page.click(LOGIN_SUBMIT_SELECTOR);
+
+  await page.waitForNavigation();
+  await saveSessionCookies(page);
+}
+
+async function getNumPeopleInMyGym(page) {
+  const numPeopleInMyGym = await page.evaluate((selector) => {
+    const element = document.querySelector(selector);
+    return element.innerHTML.split(' ')[0];
+  }, N_PEOPLE_SELECTOR);
+
+  return parseInt(numPeopleInMyGym);
+}
