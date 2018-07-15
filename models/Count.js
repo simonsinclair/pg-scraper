70e2e65a8fb693a68b475ca77f931f2be17b8d4d
Simon Sinclair <simonsinclair@users.noreply.github.com>
2018-07-15 13:57:20 +0100

    Update Count model

--- models/Count.js
+++ models/Count.js
@@ -1,18 +1,8 @@
 const mongoose = require('mongoose');
 
 const countSchema = new mongoose.Schema({
-  timestamp_hour: Date,
-
-  conditions: {
-    // Weather
-  },
-
-  // Support averaging.
-  num_samples: Number,
-  total_samples: Number,
-
-  // Data resolution - per 15 mins.
-  values: [ Number ],
+  timestamp: Date,
+  value: Number,
 });
 
 const Count = mongoose.model('Count', countSchema);
